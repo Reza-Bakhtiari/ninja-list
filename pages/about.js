f44c0a33d3897c7ddd56d41719c02140fd7f5b26
reza bakhtiari <rezabakhtiari7334@gmail.com>
2022-10-06 13:24:31 +0330

    initial commit

--- pages/about.js
+++ pages/about.js
@@ -1,40 +1,29 @@
 import Head from "next/head";
-import Image from "next/image";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-import Link from "next/link";
-import styles from "../styles/Home.module.css";
 
-export default function Home() {
+const About = () => {
   return (
     <>
       <Head>
-        <title>Ninja List | Home</title>
+        <title>Ninja List | About</title>
         <meta name="keywords" content="ninjas" />
       </Head>
       <div>
-        {/* <Navbar /> */}
-        <h1 className={styles.title}>Hello World</h1>
-        <p className={styles.text}>
+        <h1>about</h1>
+        <p>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae, animi
           quia. Culpa iure aspernatur doloremque cupiditate molestiae quas
           maxime, dolore tenetur mollitia quaerat quos et provident consequatur
           atque aliquid a?
         </p>
-        <p className={styles.text}>
+        <p>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae, animi
           quia. Culpa iure aspernatur doloremque cupiditate molestiae quas
           maxime, dolore tenetur mollitia quaerat quos et provident consequatur
           atque aliquid a?
         </p>
-        <Link href="./ninjas">
-          <a className={styles.btn}>See Ninja Listing</a>
-        </Link>
-        <br />
-        <br />
-        <br />
-        {/* <Footer /> */}
       </div>
     </>
   );
-}
+};
+
+export default About;
